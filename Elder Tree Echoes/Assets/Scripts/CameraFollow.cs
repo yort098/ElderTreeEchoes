b3d388922f9ee1059af753d2454d544cb5784d1d
yort098 <trham098@gmail.com>
2024-09-16 15:20:16 -0400

    Added basic CameraFollow script from scrapped project

---
+++ Elder Tree Echoes/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //  How much to offset the camera from the object it's
+    //  following
+    private Vector3 offset = new Vector3(0, 2f, -10f);
+
+    //  How long it takes for the camera
+    //  to reach the object it is following   
+    private float smoothTime = 0.25f; 
+
+    private Vector3 velocity = Vector3.zero;
+
+    // The object to center the camera around
+    [SerializeField]
+    private Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  The position in the scene to place the camera
+        Vector3 targetPosition = target.position + offset;
+
+        //  Smoothly moves the camera each frame to position itself to the
+        //  object its following
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
