be62c9e7f0a882940ecd4f188677a82657fd1ff3
yort098 <trham098@gmail.com>
2024-10-16 18:50:52 -0400

    Added a little animation for growing plants

--- Elder Tree Echoes/Assets/Scripts/Trampoline.cs
+++ Elder Tree Echoes/Assets/Scripts/Trampoline.cs
@@ -6,8 +6,10 @@ public class Trampoline : MonoBehaviour
 {
     public float springForce = 500;
 
+
     void OnCollisionEnter2D(Collision2D coll)
     {
+        Debug.Log("boing");
         if (coll.gameObject.tag == "Player") // Since people didn't like enemies bouncing
         {
             coll.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, springForce));
