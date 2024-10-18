b39ff07933fdd29a9c9b1311acf5f4492cafaba5
yort098 <trham098@gmail.com>
2024-10-18 00:43:56 -0400

    New build for Sprint 3

--- Elder Tree Echoes/Assets/Scripts/GameManager.cs
+++ Elder Tree Echoes/Assets/Scripts/GameManager.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UI;
 
--- Elder Tree Echoes/Assets/Scripts/Projectile.cs
+++ Elder Tree Echoes/Assets/Scripts/Projectile.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.MPE;
 using UnityEngine;
 using UnityEngine.UIElements;
 
